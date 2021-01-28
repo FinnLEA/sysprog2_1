cfe7d81ee2252a998ed90c627af81421d5dfd575
FinnLEA <56691691+FinnLEA@users.noreply.github.com>
2021-01-28 16:29:56 +0500

    new include file

--- driver/GDT_IDT_work.h
+++ driver/GDT_IDT_work.h
@@ -2,6 +2,7 @@
 #define _GDT_WORK_H_
 
 #include "intel.h"
+#include "work_includes.h" 
 
 #include <ntddk.h>
 #include <strsafe.h>
@@ -11,19 +12,10 @@
 
 #define MAKE_SELECTOR(index, RPL)   ((index << 3) + RPL)
 
-#define DUMP_GDT_IOCTL  CTL_CODE(FILE_DEVICE_UNKNOWN,0x801,METHOD_BUFFERED,FILE_READ_ACCESS | FILE_WRITE_ACCESS)
-#define DUMP_IDT_IOCTL  CTL_CODE(FILE_DEVICE_UNKNOWN,0x802,METHOD_BUFFERED,FILE_READ_ACCESS | FILE_WRITE_ACCESS)
-
-
-extern PCHAR glWriteBuffer;
-extern ULONG glSizeWriteBuffer;
 
 //-----------------------------------------------
 
-NTSTATUS DispatchControl(PDEVICE_OBJECT DeviceObject, PIRP pIrp);
-NTSTATUS CompleteIrp( PIRP Irp, NTSTATUS status,ULONG Info);
-NTSTATUS DispatchCreate(IN PDEVICE_OBJECT DeviceObject,IN PIRP pIrp);
-NTSTATUS DispatchClose(IN PDEVICE_OBJECT DeviceObject,IN PIRP pIrp);
+
 
 VOID DumpGDT (
 	OUT PVOID Buf, 
